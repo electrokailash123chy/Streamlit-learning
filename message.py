dabbcccf1146ee45eae626e3399db49d44529e08
electrookailash123chy <electrokailash@gmail.com>
2025-12-04 00:57:59 +0545

    version 2

--- message.py
+++ message.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Message Component Example")
 user_message = st.text_input("Enter your message:")
 if user_message:
-    st.write(f"You entered: {user_message}")    
+    st.write("You entered:", user_message)
