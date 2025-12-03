f4e478268bd3ddb107e1ae5172fe6dc2f7698ea9
electrookailash123chy <electrokailash@gmail.com>
2025-12-04 01:04:55 +0545

    version 3

--- message.py
+++ message.py
@@ -1,5 +1,8 @@
 import streamlit as st  
 st.title("Message Component Example")
 user_message = st.text_input("Enter your message:")
-if user_message:
-    st.write("You entered:", user_message)
+if st.button("Submit"): 
+    if user_message:
+        st.write("You entered:", user_message)
+    else:
+        st.write("Please enter a message.")
